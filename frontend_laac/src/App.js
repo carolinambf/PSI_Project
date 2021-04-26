8ed8737d5abb10087e6108a425982fd865601f62
gabriel12m <2000marcal@gmail.com>
2021-04-26 20:44:13 +0100

    Adição de uma navbar à aplicação

--- frontend_laac/src/App.js
+++ frontend_laac/src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import Mapa from "./components/Mapa";
 import "./App.css";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          LACC - Luiza Andaluz Centro de Conhecimento
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Mapa</Nav.Link>
+            <NavDropdown title="About Us" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Quem somos</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">História</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Contactos</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Mapa />
     </div>
   );
