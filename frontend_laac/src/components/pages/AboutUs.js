eb101c53a5f0f905d77205df80bd5c426e79fc52
Carolina Ferreira <aluno21071@ipt.pt>
2021-05-31 12:14:53 +0100

    texto alinhado aboutus

--- frontend_laac/src/components/pages/AboutUs.js
+++ frontend_laac/src/components/pages/AboutUs.js
@@ -4,18 +4,21 @@ import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
-    <div ClassName="sobrenos"> 
     <div className="aboutus">
-          <h2>Sobre Nós</h2>
+    <div ClassName="sobrenos"> 
+          <h2 >SOBRE NÓS</h2>
+    </div>
     <div className = "paragrafo"> 
         <p>
-        Luiza Andaluz, mulher inspiradora, audaz, culta e multifacetada, colocou a sua vida ao serviço das causas da dignidade humana e do bem comum.
-
-Suportada pelos valores humanos e cristãos, encontrou na educação integral (conhecimento, empowerment social, capacitação técnica) e simultaneamente na imprensa cristã, a forma de expandir a sua ação, em especial para com as jovens mulheres, fundando, em 1923, a Congregação das Servas de Nossa Senhora de Fátima.
+        Luiza Andaluz, mulher inspiradora, audaz, culta e multifacetada, colocou a sua vida ao serviço das 
+        causas da dignidade humana e do bem comum. Suportada pelos valores humanos e cristãos, encontrou 
+        na educação integral (conhecimento, empowerment social, capacitação técnica) e simultaneamente 
+        na imprensa cristã, a forma de expandir a sua ação, em especial para com as jovens mulheres, fundando, 
+        em 1923, a Congregação das Servas de Nossa Senhora de Fátima.
         </p>
       
       </div>
-      </div>
+      
       </div>
 
   );
