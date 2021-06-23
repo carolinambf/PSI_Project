10218e5f7b9707cc64c94e295d6a92b680a20bb9
JValente19 <aluno21073@ipt.pt>
2021-06-23 15:43:21 +0100

    Um trabalho de... continuação

--- frontend_laac/src/components/pages/Creditos.js
+++ frontend_laac/src/components/pages/Creditos.js
@@ -4,6 +4,10 @@ import val from "../../assets/val.jpg";
 import carol from "../../assets/carol.jpg";
 import ana from "../../assets/ana.jpg";
 import gabi from "../../assets/gabi.jpg";
+import jess from "../../assets/jess.jpg";
+import pedro from "../../assets/pedro.jpg";
+import joao from "../../assets/joao.jpg";
+//import rui from "../../assets/rui.jpg";
 
 const Creditos = () => {
 
@@ -11,26 +15,30 @@ const Creditos = () => {
 
         <div className="contactos">
             <div className="espacos-fisicos">
-                <h2> Créditos   PÁGINA EM CONSTRUÇÃO </h2>
-                <p></p>
+                <h2> Créditos </h2>
+                <p> </p>
             </div>
 
             <div >
-
+                <p> Um Trabalho realizado por: </p>
                 <div >
-                    <p>  &nbsp;&nbsp; &nbsp;&nbsp;   &nbsp;&nbsp; <img src={val} />
-                        &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp; <img src={carol} />
-                        &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp;  <img src={gabi} />
-                        &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp;  <img src={ana} />
+                    <p>   &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp;  <img src={gabi} />
+                          &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp; <img src={carol} />
+                          &nbsp;&nbsp; &nbsp;&nbsp;   &nbsp;&nbsp; <img src={val} />
+                          &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp;  <img src={ana} />
                     </p>
-
-
+                
+            </div>
+            <div >
+            <p>          &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp;  <img src={jess} />
+                         &nbsp;&nbsp;  &nbsp;&nbsp;  &nbsp;&nbsp; <img src={pedro} />
+                         &nbsp;&nbsp; &nbsp;&nbsp;   &nbsp;&nbsp; <img src={joao} />
+                    </p>
                 </div>
 
-            </div>
-
         </div>
+        </div>
+
     );
 }
 export default Creditos;
-
